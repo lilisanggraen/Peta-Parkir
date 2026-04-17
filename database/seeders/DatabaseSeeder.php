c9f0d56cbfbdfc3ef24f3fc1b5ef3b1dd18f7b02
Lilis Anggraeni <lilisanggraeni5206@gmail.com>
2026-04-17 09:43:03 +0700

    Menambahkan user admin ke DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,10 +8,19 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-public function run(): void
+    public function run(): void
     {
-    $this->call([
-        ParkingSpotSeeder::class,
-    ]);
+        // Membuat User Admin secara otomatis
+        User::updateOrCreate(
+            ['email' => 'admin@parkir.com'], // Cek email ini dulu
+            [
+                'name' => 'Admin Salatiga',
+                'password' => Hash::make('rahasia123'), // Password kamu
+            ]
+        );
+
+        $this->call([
+            ParkingSpotSeeder::class,
+        ]);
     }
 }
